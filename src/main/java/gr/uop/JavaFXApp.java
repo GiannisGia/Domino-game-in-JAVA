e74dbb2de3dbbb0ae5461517fa5e9d9008137f0e
Giannis Giannopoulos <dit19032@go.uop.gr>
2024-07-09 21:34:44 +0300

    Update on main Scene

--- src/main/java/gr/uop/JavaFXApp.java
+++ src/main/java/gr/uop/JavaFXApp.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,16 +18,24 @@ public class JavaFXApp extends Application {
     public void start(Stage stage) {
         var label = new Label("Welcome to Domino Game!");
         Button startGameButton = new Button("Start Game");
+        Button exitGameButton =new Button("Exit Game");
+
+        VBox mainSceneButtons = new VBox(10, startGameButton, exitGameButton);
+        mainSceneButtons.setAlignment(Pos.CENTER);
 
         StackPane mainScene = new StackPane();
-        mainScene.getChildren().addAll(label, startGameButton);
+        mainScene.getChildren().addAll(label,mainSceneButtons);
         StackPane.setAlignment(label, Pos.TOP_CENTER);
-        StackPane.setAlignment(startGameButton, Pos.CENTER);
+        StackPane.setAlignment(mainSceneButtons, Pos.CENTER);
 
         var scene = new Scene(mainScene, 640, 480);
         stage.setScene(scene);
         stage.setTitle("Domino Game");
         stage.show();
+
+        exitGameButton.setOnAction((e->{
+            stage.close();
+        }));
     }
 
     public static void main(String[] args) {
